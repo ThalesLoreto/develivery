5cd4e3fcc358043825720d904ea08fe4743e98c7
ThalesLoreto <tloreto.dev@gmail.com>
2022-03-25 15:48:32 -0300

    catch all throwed Errors aync on app

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 
 import { routes } from './routes';
 
@@ -6,5 +7,17 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+// Middleware to Catch async throw errors
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      message: err.message,
+    });
+  }
+  return res.status(500).json({
+    status: 'Error',
+    message: 'Internal Server Error',
+  });
+});
 
 export default app;
