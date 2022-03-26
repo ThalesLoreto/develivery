cc2a6109125b4a569e4a8c594e1dcf885493e643
ThalesLoreto <tloreto.dev@gmail.com>
2022-03-25 21:02:05 -0300

    create list all available deliveries endpoint

---
+++ src/modules/deliveries/useCases/findAllAvailable/FindAllAvailableUseCase.ts
@@ -0,0 +1,15 @@
+import { Deliveries } from '@prisma/client';
+
+import { prisma } from '../../../../database/prismaClient';
+
+export class FindAllAvailableUseCase {
+  async execute(): Promise<Deliveries[]> {
+    const deliveries = await prisma.deliveries.findMany({
+      where: {
+        endAt: null,
+      },
+    });
+
+    return deliveries;
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { ensureAuthenticated } from './middlewares/ensureAuthenticated';
 import { AuthenticateClientController } from './modules/clients/useCases/authenticateClients/AuthenticateClientController';
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController';
 import { CreateDeliveryController } from './modules/deliveries/useCases/createDelivery/CreateDeliveryController';
+import { FindAllAvailableController } from './modules/deliveries/useCases/findAllAvailable/FindAllAvailableController';
 import { AuthenticateDeliverymanController } from './modules/deliveryman/useCases/authenticateDeliveryman/AuthenticateDeliverymanController';
 import { CreateDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController';
 
@@ -15,6 +16,7 @@ const createDeliverymanController = new CreateDeliverymanController();
 const authenticateDeliverymanController =
   new AuthenticateDeliverymanController();
 const createDeliveryController = new CreateDeliveryController();
+const findAllAvailableController = new FindAllAvailableController();
 
 routes.post('/clients', createClientController.handle);
 routes.post('/clients/auth', authenticateClientController.handle);
@@ -26,6 +28,11 @@ routes.post(
   '/deliveries',
   ensureAuthenticated,
   createDeliveryController.handle,
+);
+routes.get(
+  '/deliveries',
+  ensureAuthenticated,
+  findAllAvailableController.handle,
 );
 
 export { routes };
